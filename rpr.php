0de2e83fefee5f06c01161342a7736e0624c9836
DAK Lights <github@daklights.com>
2024-11-29 19:50:40 -0500

    Add logging entries for debugging

--- rpr.php
+++ rpr.php
@@ -1,29 +1,29 @@
 <?php
 
 	include_once "rpr_common.php";
-	
+
 	$pluginName = basename(dirname(__FILE__));
 	$pluginConfigFile = $settings['configDirectory'] . "/plugin." .$pluginName;
 	$pluginSettings = parse_ini_file($pluginConfigFile);
-	
+
 	$deviceData = getDeviceData();
 	$playingData = getCurrentPlayingData();
 	$combined = array_merge(json_decode($deviceData,true),json_decode($playingData,true));
-	
+
 	echo "Randolph Park Reboot Data<br /><br />";
-	
+
 	echo "Temp: " . round(((($combined['tempC']/1000)*(9/5))+32),2) . "<br />";
 	echo "Serial: " . $combined['serial'] . "<br />";
 	echo "Eth0 Address: " . $combined['eth0Addr'] . "<br />";
 	echo "Wlan0 Address: " . $combined['wlan0Addr'] . "<br /><br />";
-	
+
 	echo "Device Time: " . date('Y-m-d h:i:sa',$combined['time']) . "<br />";
 	echo "Device Time Epoch: " . $combined['time'] . "<br /><br />";
 
 	echo "Current Sequence: " . $combined['currentSequence'] . "<br />";
 	echo "Elapsed Seconds: " . $combined['secondsElapsed'] . "<br />";
-	echo "Uptime Seconds: " . $combined['uptimeTotalSeconds'] . "<br /><br />";
-	
+	echo "Uptime Total Seconds: " . $combined['uptimeTotalSeconds'] . "<br /><br />";
+
 	echo "Reboot Sequences: " . (array_key_exists('rebootSequences',$pluginSettings) ? trim($pluginSettings['rebootSequences']) : '') . "<br /><br />";
 
 ?>
--- rpr_run.php
+++ rpr_run.php
@@ -1,27 +1,29 @@
 <?php
 
 	include_once "/home/fpp/media/plugins/randolphParkReboot/rpr_common.php";
-	
+
 	$doLoop = true;
 	logEntry("Randolph Park Reboot Initializing...");
-	
+
 	while ($doLoop) {
 		$pluginSettings = parse_ini_file($pluginConfigFile);
 		$deviceData = getDeviceData();
 		$playingData = json_decode(getCurrentPlayingData(),true);
 		$combined = json_encode(array_merge(json_decode($deviceData,true),$playingData));
-		
+
 		if ($playingData['mode'] == "remote") {
 			// remote mode
 			$rebootSequences = (array_key_exists('rebootSequences',$pluginSettings) ? trim($pluginSettings['rebootSequences']) : '');
-			
+
 			if ((stripos(trim($rebootSequences),trim($playingData['sequenceName'])) !== false) && ($combined['secondsElapsed'] < $combined['uptimeTotalSeconds'])) {
 				// a sequence is playing where we want to reboot remote FPP devices
-				//logEntry("Reboot Sequence Detected: " . $playingData['sequenceName']);
+				logEntry("Reboot Sequence Detected: " . $playingData['sequenceName'] . " | " . $combined['secondsElapsed'] . " | " . $combined['uptimeTotalSeconds']);
 				$result = file_get_contents('http://127.0.0.1/api/system/fppd/restart');
+				logEntry("RESULT: " . $result);
 				$sleepDuration = 30;
 			} else {
 				// a sequence is playing where we do not want to reboot remote FPP devices
+				logEntry("Do Nothing: " . $playingData['sequenceName'] . " | " . $combined['secondsElapsed'] . " | " . $combined['uptimeTotalSeconds']);
 				$sleepDuration = 30;
 			}
 		} else {
@@ -32,5 +34,5 @@
 		sleep($sleepDuration);
 
 	}
-	
+
 ?>
