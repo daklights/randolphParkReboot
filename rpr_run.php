9651841a8bc5e13835e555211cb8d55fc42c0585
DAK Lights <github@daklights.com>
2024-11-29 19:53:54 -0500

    Edit playing data source

--- rpr_run.php
+++ rpr_run.php
@@ -9,21 +9,20 @@
 		$pluginSettings = parse_ini_file($pluginConfigFile);
 		$deviceData = getDeviceData();
 		$playingData = json_decode(getCurrentPlayingData(),true);
-		$combined = json_encode(array_merge(json_decode($deviceData,true),$playingData));
 
 		if ($playingData['mode'] == "remote") {
 			// remote mode
 			$rebootSequences = (array_key_exists('rebootSequences',$pluginSettings) ? trim($pluginSettings['rebootSequences']) : '');
 
-			if ((stripos(trim($rebootSequences),trim($playingData['sequenceName'])) !== false) && ($combined['secondsElapsed'] < $combined['uptimeTotalSeconds'])) {
+			if ((stripos(trim($rebootSequences),trim($playingData['sequenceName'])) !== false) && ($playingData['secondsElapsed'] < $playingData['uptimeTotalSeconds'])) {
 				// a sequence is playing where we want to reboot remote FPP devices
-				logEntry("Reboot Sequence Detected: " . $playingData['sequenceName'] . " | " . $combined['secondsElapsed'] . " | " . $combined['uptimeTotalSeconds']);
+				logEntry("Reboot Sequence Detected: " . $playingData['sequenceName'] . " | " . $playingData['secondsElapsed'] . " | " . $playingData['uptimeTotalSeconds']);
 				$result = file_get_contents('http://127.0.0.1/api/system/fppd/restart');
 				logEntry("RESULT: " . $result);
 				$sleepDuration = 30;
 			} else {
 				// a sequence is playing where we do not want to reboot remote FPP devices
-				logEntry("Do Nothing: " . $playingData['sequenceName'] . " | " . $combined['secondsElapsed'] . " | " . $combined['uptimeTotalSeconds']);
+				logEntry("Do Nothing: " . $playingData['sequenceName'] . " | " . $playingData['secondsElapsed'] . " | " . $playingData['uptimeTotalSeconds']);
 				$sleepDuration = 30;
 			}
 		} else {
